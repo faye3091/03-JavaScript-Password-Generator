de755845725517395b4266463a93c1442f804d02
Vanessa <faye_3091@yahoo.com>
2022-08-30 00:04:34 -0400

    developed the javascript and created readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # 03 JavaScript: Password Generator
 
-## Your Task
+## Description
 
-This week's Challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
-
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+```
+This application generates a random password based on user-selected criteria. This app runs in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code.
+```
 
 ## User Story
 
@@ -40,61 +40,30 @@ The following image shows the web application's appearance and functionality:
 
 ![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
 
-## Grading Requirements
+## Technologies used
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
+```
+HTML
+CSS
+JavaScript
+```
 
-This Challenge is graded based on the following criteria: 
+## Resources
 
-### Technical Acceptance Criteria: 40%
+```
+https://net-comber.com/charset.html
 
-* Satisfies all of the preceding acceptance criteria.
+https://www.w3schools.com/jsref/jsref_fromcharcode.asp
+```
 
-### Deployment: 32%
+## Link to deployed application
 
-* Application deployed at live URL.
+```
 
-* Application loads with no errors.
+```
 
-* Application GitHub URL submitted.
+## Repository link
 
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+```
+https://github.com/faye3091/03-JavaScript-Password-Generator
+```
--- script.js
+++ script.js
@@ -1,14 +1,123 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength;
+
+//Created a object for the functions
+const randomFunc = {
+  confirmLower: getRandomLower,
+  confirmUpper: getRandomUpper,
+  confirmNumber: getRandomNumber,
+  confirmSymbol: getRandomSymbol,
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//generate password function
+function generatePassword() {
+  //Ask for User input
+  passwordLength = prompt(
+    "How many charactors would you like your password? Choose between 8 and 128"
+  );
+  console.log("Password length " + passwordLength);
+
+  if (!passwordLength) {
+    alert("Required Value! Please retry");
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You must choose between 8 to 128");
+    console.log("Password length " + passwordLength);
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Error! Please try retry again!");
+      return;
+    } else {
+      confirmLower = confirm("Will this conrtain Lower case letters?");
+      confirmUpper = confirm("Will this contain Upper case Letters?");
+      confirmNumber = confirm("Will this contain Numbers?");
+      confirmSymbol = confirm("Will this contain Special characters?");
+    }
+  } else {
+    confirmLower = confirm("Will this conrtain Lower case letters?");
+    confirmUpper = confirm("Will this contain Upper case Letters?");
+    confirmNumber = confirm("Will this contain Numbers?");
+    confirmSymbol = confirm("Will this contain Special characters?");
+  }
+
+  let generatedPassword = " ";
+
+  const typesCount =
+    confirmLower + confirmUpper + confirmNumber + confirmSymbol;
+
+  console.log("typesCount:", typesCount);
+  //created an array of objects that have these values as the key, ex: confirmLower: true and etc. by adding the {} symbols
+  //I used .filter to take out the false in the confirmed values.
+  const typesArr = [
+    { confirmLower },
+    { confirmUpper },
+    { confirmNumber },
+    { confirmSymbol },
+  ].filter((item) => Object.values(item)[0]);
+  //We loop through each item and based on true or false value it will filter out equals false.
+
+  console.log("typesArray: ", typesArr);
+
+  //This is to check if there is no confirmed values then don't proceed or do nothing
+  if (typesCount === 0) {
+    return "";
+  }
+
+  //generating different kinds of the confirmed values
+  //Loope over length call generator function for each type
+
+  //i += typesCount because I want to increment it by the number of confirmed values(typesCount)
+  for (let i = 0; i < passwordLength; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  console.log(generatedPassword);
+  return generatedPassword;
+}
+
+//Generator functions
+
+//Generates a random lower case letter
+function getRandomLower() {
+  //String.fromCharCode() is a codes for all the characters including lower cases and numbers
+  //sample: String.fromCharCode(97) generates the lower case a.
+  //in order to get all lower cases I need 97 to 122
+  //26 because there are 26 letters in the alphabet
+  //Math.floor is to generate a whole number beacause Math.random() is all decimals
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+//Generates a random Upper case letter
+function getRandomUpper() {
+  //Here again I am using String.fromCharcode to get the Upper cases
+  //In this case number 65 to 90 (still 26 letters)
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+//Generates a random number
+function getRandomNumber() {
+  //Here again I am using String.fromCharcode to get the numbers
+  //In this case number 48 to 57 (from 0 to 9) so it will be a span 10
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+//Generates a random symbol
+function getRandomSymbol() {
+  //This are a string of symbols that are accepted as password
+  var symbols = ' !@#$%^&*(){}[]=<>/,."+-:;?`|~';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
